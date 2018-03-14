97578d638a897912ac25da357814b8058bc0bcac
yaoguilv <yaoguilv@gmail.com>
2018-03-14 13:41:28 +0800

    modify

--- Cal/Al/spike/ticket.cpp
+++ Cal/Al/spike/ticket.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <iterator>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
+
+int fibonacci(int n)
+{
+    if(n < 2)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n -2);
+}
 
 int main(int argc, char ** argv)
 {
-    cout << to_string(223) << endl;
+    system_clock::time_point start = system_clock::now();
+    fibonacci(42);
+    system_clock::time_point end = system_clock::now();
+    duration<double> elapsed_seconds = end - start;
+    cout << elapsed_seconds.count() << endl;
+
     return 0;
 }
