babf203f58c412674f1a7453cb845fb53a163242
yaoguilv <yaoguilv@gmail.com>
2018-06-11 15:24:01 +0900

    modify MSD.java

--- Jal/Al/test/Tester.java
+++ Jal/Al/test/Tester.java
@@ -2,10 +2,13 @@ package test;
 
 import StdLib.*;
 import C5.U1.Alphabet;
-import C5.U1.LSD;
+import C5.U1.MSD;
 
 public class Tester {
     public static void main(String[] args)
     {
+
+
+
     }
 }
