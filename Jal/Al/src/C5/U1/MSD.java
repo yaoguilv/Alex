656f63c0f3e2c4a7d7e7083558ac9fcc65e81588
yaoguilv <yaoguilv@gmail.com>
2018-06-12 15:16:40 +0900

    MSD(String Sort) java

--- Jal/Al/src/C5/U1/MSD.java
+++ Jal/Al/src/C5/U1/MSD.java
@@ -1,5 +1,7 @@
 package C5.U1;
 
+import C2.U1.Insertion;
+
 public class MSD {
     // radix
     private static int R = 256;
@@ -28,7 +30,7 @@ public class MSD {
         // Sort form a[lo] to a[hi], starting at the dth character.
         if(hi <= lo + M)
         {
-            Insertion.sort(a, lo, hi, d);
+            insertion(a, lo, hi, d);
             return;
         }
 
@@ -52,4 +54,25 @@ public class MSD {
         for(int r = 0; r < R; r++)
             sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1);
     }
+
+    // return dth character of s, -1 if d = length of string
+    private static void insertion(String[] a, int lo, int hi, int d) {
+        for (int i = lo; i <= hi; i++)
+            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
+                exch(a, j, j-1);
+    }
+
+    // exchange a[i] and a[j]
+    private static void exch(String[] a, int i, int j) {
+        String temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    // is v less than w, starting at character d
+    private static boolean less(String v, String w, int d) {
+        assert v.substring(0, d).equals(w.substring(0, d));
+        return v.substring(d).compareTo(w.substring(d)) < 0;
+    }
+
 }
--- Jal/Al/test/Tester.java
+++ Jal/Al/test/Tester.java
@@ -1,14 +1,16 @@
 package test;
 
 import StdLib.*;
-import C5.U1.Alphabet;
 import C5.U1.MSD;
 
 public class Tester {
     public static void main(String[] args)
     {
+        String[] a = In.readStrings(args[0]);
 
-
-
+        int N = a.length;
+        MSD.sort(a);
+        for (int i = 0; i < N; i++)
+            StdOut.println(a[i]);
     }
 }
